f9320c182086eed9471d5358ec5ece264b2eb4a0
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-01-26 11:18:07 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,12 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray (array, element) {
-  var newarr = array
+  var newarr = array;
   newarr.unshift(element);
   return newarr;
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  array.unshift(element);
+  return array;
 }
