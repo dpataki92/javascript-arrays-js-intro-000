c75a81cf718f7c8dde9a4cd8d1514d38945e416e
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-01-26 10:46:36 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,21 @@
+var chocolateBars = [snickers, hundred grand, kitkat, skittles]
+
+function addElementToBeginningOfArray (arr, el) {
+  var new = arr.unshift(el);
+  return new;
+}
+
+function destructivelyAddElementToBeginningOfArray (arr, el) {
+  arr.unshift(el);
+  return arr;
+}
+
+function addElementToEndOfArray (arr, el) {
+  var new = arr.push(el);
+  result new;
+}
+
+function destructivelyAddElementToEndOfArray () {
+  arr.push(el);
+  return arr;
+} 
